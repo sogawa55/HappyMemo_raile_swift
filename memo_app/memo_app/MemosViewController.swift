778879ebdebccc2270711dffa649ef564e68e1aa
sogawa55 <s.ogawa055@gmail.com>
2017-08-03 23:27:56 +0900

    add comments

--- memo_app/memo_app/MemosViewController.swift
+++ memo_app/memo_app/MemosViewController.swift
@@ -1,10 +1,4 @@
-//
-//  MemosViewController.swift
-//  memo_app
-//
-//  Created by shohei on 2017/05/07.
-//  Copyright © 2017年 ogawa.shoei. All rights reserved.
-//
+
 
 import UIKit
 import Foundation
@@ -13,7 +7,7 @@ class MemosViewController: UIViewController,UITableViewDelegate, UITableViewData
 
     
     @IBOutlet var memosTableView: UITableView!
-    
+    //ダミー
     var memos:[Memo] =  [Memo(title: "hoge", body: "hogehoge"),
                          Memo(title: "foo", body: "fooooo"),
                          Memo(title: "bar", body: "barbar")]
@@ -29,10 +23,9 @@ class MemosViewController: UIViewController,UITableViewDelegate, UITableViewData
     }
 
         
-        
-   
+       
  override func viewDidLoad() {
-    
+    　　//registerメソッドでテーブルビューのセルを生成、再利用設定
         memosTableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
     
  }
@@ -41,14 +34,14 @@ class MemosViewController: UIViewController,UITableViewDelegate, UITableViewData
 
     
 
-    
+ //画面の表示直前に実行   
 override func viewWillAppear(_ animated: Bool) {
         refreshData()
     }
     
     
     func refreshData(){
-    
+    //クラスメソッドを実行
     Memo.getMemos(
     success: {(memos) -> Void in
     self.memos = memos.reversed()
@@ -73,40 +66,43 @@ override func viewWillAppear(_ animated: Bool) {
  }
 
 
-
-    
-    
-    
-    
+//メモの数だけセクションごとの行数を返す
 func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return memos.count
     }
 
+//各行に表示するセルを生成する。
 func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath as IndexPath)
     
     let memo = memos[indexPath.row]
+    
+    //セルにタイトルを設定
     cell.textLabel?.text =  memo.title
     
     return cell
 
  }
     
-    
+    //セルを選択したら詳細画面に遷移
     func tableView(_ table: UITableView,didSelectRowAt indexPath: IndexPath) {
     performSegue(withIdentifier: "showDetail",sender: nil)
     }
     
+    
+    //削除ボタンの表示
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             
             let memo = self.memos[indexPath.row]
             
-
+            //クラスメソッドの実行
             memo.deleteMemo(
                 success: {
                     print("success delete")
+                    //選択されたデータを削除
                     self.memos.remove(at: indexPath.row)
+                    //選択された行を削除
                     tableView.deleteRows(at: [indexPath as IndexPath], with: .fade)
             },
                 failure: {(error) in
@@ -120,16 +116,20 @@ func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> U
     
 
     
-    
+    //画面遷移前に実行
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
+        //遷移先の詳細画面をcontroller変数に格納
         guard let controller = segue.destination as?MemoDetailViewController else{
             return}
         if segue.identifier == "showDetail" {
             
             if let indexPath = self.memosTableView.indexPathForSelectedRow {
+                //選択されたセルの行番号を格納
                 let memo = memos[indexPath.row]
+                //MemoDetailViewContorollerのmemoプロパティに、選択されたセルのデータを格納
                 controller.memo = memo
+                //ナビゲーションボタンの変更
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem
                 controller.navigationItem.leftItemsSupplementBackButton = true
                 }
