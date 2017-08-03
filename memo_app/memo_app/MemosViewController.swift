6df9f7ec67e0dbd977c70f4589f1b1b0c8c4716d
sogawa55 <s.ogawa055@gmail.com>
2017-08-03 23:29:48 +0900

    add comment

--- memo_app/memo_app/MemosViewController.swift
+++ memo_app/memo_app/MemosViewController.swift
@@ -71,7 +71,7 @@ func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> 
         return memos.count
     }
 
-//各行に表示するセルを生成する。
+//各行に表示するセルを返す
 func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath as IndexPath)
     
