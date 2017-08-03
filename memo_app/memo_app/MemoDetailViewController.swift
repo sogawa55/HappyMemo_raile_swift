49b47f377e4b85b3157d66d2894bc2bc2a0eed9f
sogawa55 <s.ogawa055@gmail.com>
2017-08-03 22:41:41 +0900

    add comments

--- memo_app/memo_app/MemoDetailViewController.swift
+++ memo_app/memo_app/MemoDetailViewController.swift
@@ -1,36 +1,21 @@
-//
-//  ViewController.swift
-//  memo_app
-//
-//  Created by shohei on 2017/05/07.
-//  Copyright © 2017年 ogawa.shoei. All rights reserved.
-//
-
 import UIKit
-
-
 
 
 class MemoDetailViewController: UIViewController {
     
     var memo : Memo?
     
-    
-
+    //タイトルテキストをOutlet接続
     @IBOutlet weak var titleText: UILabel!
-    
+    //本文テキストをOutlet接続
     @IBOutlet weak var bodyText: UILabel!
-    
-    
-
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         if let memo = self.memo {
-         
+         　　
             titleText.text = memo.title
           
             bodyText.text = memo.body
